996ed608bf8b1be2356298b03ede0681de32cfd3
Damo <27757251@qq.com>
2018-11-26 02:06:29 +0800

    增加了读取枚举的代码

--- Game_FireEmblem.cpp
+++ Game_FireEmblem.cpp
@@ -1,9 +1,15 @@
 #include"Game_FireEmblem.h"
-#include<stdio.h>
+
+#include"LuaState.h"
 
 Game* Game::newGame(){return new Game_FireEmblem();}
 
-Game_FireEmblem::Game_FireEmblem(){}
+Game_FireEmblem::Game_FireEmblem(){
+	LuaState state;
+	state.doFile("datas/Enum.lua");//打开枚举文件
+	state.readEnum("WeaponType",weaponType);//读取武器类型枚举
+	state.readEnum("Attributes",attributes);//读取属性枚举
+}
 Game_FireEmblem::~Game_FireEmblem(){}
 
 void Game_FireEmblem::consumeTimeSlice(){}
--- Game_FireEmblem.h
+++ Game_FireEmblem.h
@@ -2,15 +2,17 @@
 #define GAME_FIREEMBLEM_H
 
 #include"Game.h"
-#include"LuaState.h"
+#include"EnumType.h"
 
+//游戏模块本体,负责整体管理
 class Game_FireEmblem:public Game{
 public:
 	Game_FireEmblem();
 	~Game_FireEmblem();
 protected:
 	void consumeTimeSlice();
-	LuaState *enumState;
+	EnumType weaponType;//武器类型
+	EnumType attributes;//属性类型
 };
 
 #endif
